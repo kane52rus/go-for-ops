9bbe5d89b43ac5b1dcbdfdba19371c4db6105be8
Atem Karpov <art.frela@gmail.com>
2022-08-19 22:11:41 +0000

    Update Exercises/4.Concurrency/4/main_test.go

--- Exercises/4.Concurrency/4/main_test.go
+++ Exercises/4.Concurrency/4/main_test.go
@@ -43,7 +43,8 @@ func Test_realMain(t *testing.T) {
 				defer wg.Done()
 				realMain(tt.a, tt.b, &c)
 			}()
-
+			wg.Wait()
+			
 			if !reflect.DeepEqual(tt.want, c) {
 				t.Errorf("realMain() c = %v, want %v", c, tt.want)
 			}
