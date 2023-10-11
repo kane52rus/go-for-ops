425e28be7d9b365cbf1cbd7258183a86c27aaf14
Andrey Tabunkin <skull2010@yandex.ru>
2023-10-12 00:20:56 +0300

    2.2 task completed

--- Exercises/2.Basics/2/main.go
+++ Exercises/2.Basics/2/main.go
@@ -1,10 +1,46 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Тут демонстрация работы функции
+	listOfString := []string{"a", "ab", "b", "cc", "bc", "c", "cd"}
+	fmt.Println(isSorted(listOfString))
 }
 
 func isSorted(ww []string) bool {
-	// TODO: код писать здесь
+	if len(ww) == 0 {
+		return false
+	}
+	for k, v := range ww {
+		if k == 0 {
+			continue
+		} else {
+			currentValue := v
+			previousValue := ww[k-1]
+			currentValueLength := len(currentValue)
+			previousValueLength := len(previousValue)
+			var maxValue, minValue string
+			if currentValueLength > previousValueLength {
+				maxValue = currentValue
+				minValue = previousValue
+			} else {
+				minValue = currentValue
+				maxValue = previousValue
+			}
+			for m, _ := range minValue {
+				minValueLetter := minValue[m]
+				maxValueLetter := maxValue[m]
+				if minValueLetter >= maxValueLetter {
+					continue
+				} else if currentValueLength > previousValueLength {
+					continue
+				} else {
+					return false
+				}
+			}
+		}
+	}
 	return true
 }
